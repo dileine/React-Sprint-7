8ff563d1dbd230a28d8a5207326543513f432b31
dileine <mar.audiovisuals@gmail.com>
2023-06-12 22:04:25 +0200

    Ex1: useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [webPage, setWebPage] = useState(false);
+  const [seoInquiry, setSeoInquiry] = useState(false);
+  const [addCampaign, setAddCampaign] = useState(false);
+
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+
+    if (name === "webPage") {
+      setWebPage(checked);
+    } else if (name === "seoInquiry") {
+      setSeoInquiry(checked);
+    } else if (name === "addCampaign") {
+      setAddCampaign(checked);
+    }
+  };
+
+  useEffect(() => {
+    let totalPrice = 0;
+
+    if (webPage) {
+      totalPrice += 500;
+    }
+
+    if (seoInquiry) {
+      totalPrice += 300;
+    }
+
+    if (addCampaign) {
+      totalPrice += 200;
+    }
+
+    setTotalPrice(totalPrice);
+  }, [webPage, seoInquiry, addCampaign]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>¿Qué quieres hacer?</p>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="webPage"
+            checked={webPage}
+            onChange={handleCheckboxChange}
+          />
+          Una página Web (500€)
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="seoInquiry"
+            checked={seoInquiry}
+            onChange={handleCheckboxChange}
+          />
+          Una consulta SEO (300€)
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="addCampaign"
+            checked={addCampaign}
+            onChange={handleCheckboxChange}
+          />{" "}
+          Una campaña de Google Adds (200€)
+        </label>
+      </div>
+      <p>Precio: {totalPrice}</p>
     </div>
   );
 }
